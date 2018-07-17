b0d28610965a271adbe6064683e230698276ccd9
ahmed <ahmedragheb75@gmail.com>
2018-07-17 15:05:00 +0300

    change generated giftcard to barcode image

--- giftcard/giftcard/doctype/gift_card/gift_card.js
+++ giftcard/giftcard/doctype/gift_card/gift_card.js
@@ -2,28 +2,8 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Gift Card', {
-	refresh: function(frm) {
+    refresh: function(frm) {
 
-	}
+    }
 
 });
-
-cur_frm.cscript.validate = function(doc, dt, dn) { 
-		var ww = $("[data-fieldname=card_number]");
-		var svg = ww.find('svg')[0];
-		JsBarcode(svg, makeid()  , {height: 40});
-
-		cur_frm.doc.refresh("card_number");
-	 }
-
-
-function makeid() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (var i = 0; i < 9 ; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-  return text;
-}
-
--- giftcard/giftcard/doctype/gift_card/gift_card.py
+++ giftcard/giftcard/doctype/gift_card/gift_card.py
@@ -12,13 +12,13 @@ from frappe.utils.data import flt
 class GiftCard(Document):
 	
 	def validate(self):
-		# if not self.card_number:
-			# self.create_card_number()
-			# frappe.throw('please enter card_number')
-		if flt(self.amount) <=0:
-			frappe.throw("Amount Should be Positive number")
-		if not self.balance:
-			self.balance = self.amount
+ 		if not self.card_number:
+ 			self.create_card_number()
+ 		if flt(self.amount) <=0:
+ 			frappe.throw("Amount Should be Positive number")
+ 		if not self.balance:
+ 			self.balance = self.amount
+
 
 	def update_balance(self,x):
 		if self.balance < x :
@@ -39,9 +39,10 @@ class GiftCard(Document):
 
 	# ignored for using js lib
 	def create_card_number(self):
-		data = list(string.ascii_lowercase)
+		# data = list(string.ascii_lowercase)
+		data = []
 		[data.append(n) for n in range(0, 10)]
-		random_list = [str(data[randint(0, len(data)-1)]) for n in range(0, 7)]
+		random_list = [str(data[randint(0, len(data)-1)]) for n in range(0, 11)]
 		random_string = ''.join(random_list)
 		self.card_number = random_string
 
